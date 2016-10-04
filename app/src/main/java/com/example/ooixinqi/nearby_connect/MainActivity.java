92f3477a4d7cddb4cda15d91e88acd9baba748c5
xinqi001 <xinqiooi1112@gmail.com>
2016-10-04 16:38:39 +0800

    Initial commit - Get user consent about Nearby API

---
+++ app/src/main/java/com/example/ooixinqi/nearby_connect/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ooixinqi.nearby_connect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.Messages;
+import com.google.android.gms.nearby.messages.PublishCallback;
+import com.google.android.gms.nearby.messages.PublishOptions;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeCallback;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
+
+import android.util.Log;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("TAG", "GoogleApiClient connected");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("Exception:", connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("Connection suspended:", String.valueOf(i));
+    }
+
+}
